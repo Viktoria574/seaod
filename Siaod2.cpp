f1a69bb5200eb295a33537bbdaea284334059d23
Viktoria Poliakova <viktoria.v.poliakova@gmail.com>
2023-02-27 23:09:15 +0300

    Generation done

--- Siaod2.cpp
+++ Siaod2.cpp
@@ -2,12 +2,30 @@
 
 using namespace std;
 
-struct Node{
+struct MyNode{
     int data;
-    Node *next;//указатель на следущий элемент
-    Node(int data, Node *next)
+    MyNode *next;//указатель на следущий элемент
+};
+
+void setList(MyNode *L, int n){
+    MyNode *temp, *el=NULL;
+    for (int i=0; i<n; i++){
+        temp->data=1 + (rand() % 9);
+        temp->next=NULL;
+        el=temp;
+        if(i==0){
+            L=temp;
+        }
+        else{
+            el->next=temp;
+        }
+        el=temp;
+    }
 }
 
+
+
 int main(){
-    Node L;
+    MyNode *L;
+    setList(L, 10);
 };
